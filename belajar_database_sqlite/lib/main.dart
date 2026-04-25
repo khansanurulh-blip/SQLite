670e9516355ef0d42c1b892568c0c51e6910c4c7
khansanurulh-blip <khansanurulh@gmail.com>
2026-04-25 21:19:49 +0700

    tugas UTS

--- belajar_database_sqlite/lib/main.dart
+++ belajar_database_sqlite/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart' as p;
 
 void main() {
   runApp(const MainApp());
@@ -15,6 +17,68 @@ class MainApp extends StatelessWidget {
   }
 }
 
+class DatabaseHelper {
+  static Database? _database;
+
+  static Future<Database> get database async{
+    if(_database != null) return _database!;
+    _database = await _initDB();
+    return _database!;
+  }
+
+  static Future<Database?> _initDB() async {
+    String path = p.join(await getDatabasesPath(), "user_db.db");
+
+    return await openDatabase(
+      path, 
+      version: 1, 
+      onCreate: (db, version) {
+        return db.execute(
+          "CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, nama TEXT, umur INTEGER)",
+        );
+      },
+    );
+  }
+
+  //create
+  static Future<int> insertData(UserModel userModel) async {
+    final db = await database;
+    Map<String, dynamic> user = userModel.toJson();
+
+    return await db.insert(
+      "users",
+      user,
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  //read
+  static Future<List<UserModel>> getData() async {
+    final db = await database;
+    List<Map<String, Object?>> result = await db.query("users");
+
+    List<UserModel> users = result.map((userMap) {
+      return UserModel.fromJson(userMap);
+    }).toList();
+
+    return users;
+  }
+
+  //update
+  static Future<int> updateData(int id, UserModel userModel)async{
+    final db = await database;
+    var user = userModel.toJson()..remove("id");
+
+    return await db.update("users", user, where: "id=?", whereArgs: [id]);
+  }
+
+  //delete
+  static Future<int> deleteData(int id) async{
+    final db = await database;
+    return await db.delete("users", where:"id=?", whereArgs: [id]);
+  }
+}
+
 class ListUserDataPage extends StatefulWidget {
   const ListUserDataPage({super.key});
 
@@ -28,6 +92,17 @@ class UserModel {
   int umur = 0;
 
   UserModel(this.id, {required this.nama, required this.umur});
+
+  // convert dari map / hashmap ke model
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(json["id"], nama: json["nama"], umur: json["umur"]);
+  }
+
+// convert dari model ke map
+
+  Map<String, dynamic> toJson() {
+    return {"id": id, "nama": nama, "umur": umur,};
+  }
 }
 
 class _ListUserDataPageState extends State<ListUserDataPage> {
@@ -37,12 +112,23 @@ class _ListUserDataPageState extends State<ListUserDataPage> {
   final Color primaryBlue = Colors.lightBlue;
   final Color softBlue = Colors.lightBlueAccent.withOpacity(0.2);
 
-  List<UserModel> userList = [
-    UserModel(1, nama: "Khansa", umur: 20),
-    UserModel(2, nama: "Hasbi", umur: 19),
-    UserModel(3, nama: "Cahaya", umur: 17),
-    UserModel(4, nama: "Ameera", umur: 13),
-  ];
+  List<UserModel> userList = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _reloadData();
+  }
+
+  void _reloadData() async{
+    var users = await DatabaseHelper.getData();
+
+    setState(() {
+      userList = users;
+    });
+  }
 
   void _form(int? id) {
     if (id != null) {
@@ -144,21 +230,17 @@ class _ListUserDataPageState extends State<ListUserDataPage> {
     );
   }
 
-  void _save(int? id, String nama, int umur) {
+  void _save(int? id, String nama, int umur) async {
+    var newUser = UserModel(null, nama: nama, umur: umur);
     if (id != null) {
-      var user = userList.firstWhere((data) => data.id == id);
-      setState(() {
-        user.nama = nama;
-        user.umur = umur;
-      });
+      await DatabaseHelper.updateData(id, newUser);
+
     } else {
-      var nextId = userList.length + 1;
-      var newUser = UserModel(nextId, nama: nama, umur: umur);
-      setState(() {
-        userList.add(newUser);
-      });
+      
+      await DatabaseHelper.insertData(newUser);
     }
 
+    _reloadData();
     Navigator.pop(context);
   }
 
@@ -189,9 +271,9 @@ class _ListUserDataPageState extends State<ListUserDataPage> {
             style: TextButton.styleFrom(
               backgroundColor: primaryBlue.withOpacity(0.7),
             ),
-            onPressed: () {
-              setState(() =>
-                  userList.removeWhere((data) => data.id == id));
+            onPressed: () async {
+              await DatabaseHelper.deleteData(id);
+              _reloadData();
               Navigator.pop(context);
             },
             child: const Text(
